a889a7a7544069d3efe3987dd0a7b27b7f8cb9b5
Idah <idah.koome@strathmore.edu>
2020-08-13 19:34:42 +0300

    Small changes

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -2,5 +2,5 @@
 
 export default function getBaseUrl(){
   const inDevelopment = window.location.hostname === 'localhost';
-  return inDevelopment ? 'http://localhost:3001/' : '';;
+  return inDevelopment ? 'http://localhost:3001/' : '/';
 }
