17cd4fb64243c3922df60a72856f3ee9511aad46
Idah <idah.koome@strathmore.edu>
2020-08-13 22:26:50 +0300

    Configured dist for production

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,6 +1,19 @@
 /* Checks the URL to determine if the app is running on dev or prod*/
 
 export default function getBaseUrl(){
-  const inDevelopment = window.location.hostname === 'localhost';
-  return inDevelopment ? 'http://localhost:3001/' : '/';
+  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' :'/';
+  // const inDevelopment = window.location.hostname === 'localhost';
+  // return inDevelopment ? 'http://localhost:3001/' : '/';
+
+function getQueryStringParameterByName(name, url){
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=(^&#|*)|&|#|$)"),
+     results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+
 }
