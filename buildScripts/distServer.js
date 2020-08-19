0586acc7d7033275fc2856cb36f2dfff6467f8f3
Idah <idah.koome@strathmore.edu>
2020-08-19 21:10:00 +0300

    Set up automated API Deploy

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -15,15 +15,6 @@ app.get('/',function(req, res){
    res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
-app.get('/users',function(req, res){
-  //this info should be on a database
-  res.json([
-    {"id":1,"firstName":"Idah","lastName":"Smith","email":"idah@gmail.com"},
-    {"id":2,"firstName":"Tom","lastName":"Brown","email":"brownttom@gmail.com"},
-    {"id":3,"firstName":"Jane","lastName":"Smith","email":"jjane@gmail.com"}
-  ]);
-});
-
 app.listen(port , function (err) {
   if (err) {
     console.log(err);
